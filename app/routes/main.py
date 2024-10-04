cda3ba9c36e8097547a9d332213ec429fce95690
wilfredhuang <53524711+wilfredhuang@users.noreply.github.com>
2024-10-04 22:11:51 +0800

    Small Add on to earlier integration

--- app/routes/main.py
+++ app/routes/main.py
@@ -35,6 +35,20 @@ def index():
 
     return render_template('index.html', **render_variables)
 
+
+@main_bp.route('/stock')
+def stock_page():
+    secret_key = current_app.config.get('SECRET_KEY', 'default123')
+    # Define render variables
+    render_variables = {
+        'secret_key': secret_key,
+        'user_name': 'John Doe',
+    }
+    print("Hello World")
+
+    return render_template('index_stock.html', **render_variables)
+
+
 @main_bp.route('/fetch-stock-data', methods=['POST'])
 def fetch_stock_data():
     try:
